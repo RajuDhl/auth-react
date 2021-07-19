bc9bd516e3b4d5a0b69812d40a09262c057c827b
raju <dahalraju55@gmail.com>
2021-07-19 23:41:22 +0545

    Nav and routing

--- src/App.tsx
+++ src/App.tsx
@@ -2,15 +2,25 @@ import React from 'react';
 import './App.css';
 import Nav from "./Components/Nav";
 import Login from "./pages/Login";
+import {BrowserRouter, Route} from "react-router-dom";
+import Home from "./pages/Home";
+import Register from "./pages/Register";
 
 function App() {
   return (
     <div className="App">
+        <BrowserRouter>
         <Nav />
 
         <main className="form-signin">
-            <Login />
+
+                <Route path="/" exact component={Home} />
+                <Route path="/Login" exact component={Login} />
+                <Route path="/Register" exact component={Register} />
+
+
       </main>
+        </BrowserRouter>
     </div>
   );
 }
--- src/Components/Nav.tsx
+++ src/Components/Nav.tsx
@@ -1,18 +1,19 @@
 import React from 'react';
+import { Link } from 'react-router-dom';
 
 const Nav = () => {
     return (
         <nav className="navbar navbar-expand-md navbar-dark bg-dark mb-4">
             <div className="container-fluid">
-                <a className="navbar-brand" href="#">Home</a>
+                <Link to="" className="navbar-brand">Home</Link>
 
-                <div className="collapse navbar-collapse" id="navbarCollapse">
-                    <ul className="navbar-nav me-auto mb-2 mb-md-0">
+                <div>
+                    <ul className="d-flex navbar-nav me-auto mb-2 mb-md-0">
                         <li className="nav-item">
-                            <a className="nav-link active" aria-current="page" href="#"> Login</a>
+                            <Link to="/Login" className="nav-link active" aria-current="page" href="#"> Login</Link>
                         </li>
                         <li className="nav-item">
-                            <a className="nav-link active" aria-current="page" href="#"> Sign Up </a>
+                            <Link to="/Register" className="nav-link active" aria-current="page" href="#"> Sign Up</Link>
                         </li>
                     </ul>
                 </div>
